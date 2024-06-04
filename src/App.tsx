3dd927e4f552b5f74eb2eb3b632f57fa678b09bf
Ludovit <kopcsanyiludovit@gmail.com>
2024-06-04 13:07:45 +0200

    Added section component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Section from './components/Section'
 
 function App() {
 
   return (
     <>
+      <Section />
       <h1>Kopcsányi</h1>
     </>
   )
---
+++ src/components/Section.tsx
@@ -0,0 +1,11 @@
+const Section = () => {
+
+	return (
+		<div className='section h-screen w-screen'>
+			Section
+		</div>
+	)
+
+}
+
+export default Section
